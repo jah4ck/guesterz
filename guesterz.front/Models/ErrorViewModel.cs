7a7a65ca8a6e18cb897a797cd32fac9b7bb8ee16
jahack <johann.simon.web@gmail.com>
2017-11-01 10:51:57 +0100

    ok

---
+++ guesterz.front/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace guesterz.front.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
